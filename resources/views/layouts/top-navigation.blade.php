e4af4d7ed18c3b32e5b1fcd25b45554b8692a67f
Zilphixx <144513145+Zilphixx@users.noreply.github.com>
2024-09-24 00:05:05 +0800

    fixed dashboard nav link display checks

--- resources/views/layouts/top-navigation.blade.php
+++ resources/views/layouts/top-navigation.blade.php
@@ -17,7 +17,13 @@
                 @auth
                     <!-- Navigation Links -->
                     <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                        @if(Auth::user()->user_type === 'Teacher' && Auth::user()->teacherDetail->is_verified)
+                        @if(Auth::user()->user_type === 'Teacher')
+                            @if(Auth::user()->teacherDetail->is_verified)
+                                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                                    {{ __('Dashboard') }}
+                                </x-nav-link>
+                            @endif
+                        @else
                         <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                             {{ __('Dashboard') }}
                         </x-nav-link>
